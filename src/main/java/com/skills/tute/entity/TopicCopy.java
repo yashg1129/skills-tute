9a382128e17a5d48544e4748677e1ce0531fc0cf
Ravindra <yash1129@yahoo.com>
2026-03-25 11:55:43 +0530

    Merge

---
+++ src/main/java/com/skills/tute/entity/TopicCopy.java
@@ -0,0 +1,28 @@
+package com.skills.tute.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "topics")
+public class TopicCopy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "display_order", nullable = false)
+    private Integer displayOrder;
+
+}

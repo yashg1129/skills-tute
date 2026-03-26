90535dc7a175dad66212d5ca7fa273e1b068864d
Ravindra <yash1129@yahoo.com>
2026-03-26 13:12:12 +0530

    Code clean

--- src/app/layout/footer/footer.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-footer',
-  standalone: true,
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
-})
-export class FooterComponent {}
--- src/app/pages/spring-boot-tutorial/spring-boot-tutorial.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-spring-boot-tutorial',
-  standalone: true,
-  templateUrl: './spring-boot-tutorial.component.html',
-  styleUrl: './spring-boot-tutorial.component.scss'
-})
-export class SpringBootTutorialComponent {}
